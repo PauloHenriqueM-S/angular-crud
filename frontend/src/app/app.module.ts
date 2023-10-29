51d9e6ea49bcd90a73c403c2efc113256c01a9a4
Paulo Henrique <phttm0501@gmail.com>
2023-10-28 20:39:57 -0400

    product-update component created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MatSortModule } from "@angular/material/sort";
 import localePt from "@angular/common/locales/pt";
 import { registerLocaleData } from "@angular/common";
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeletComponent } from './components/products/product-delet/product-delet.component';
 
 registerLocaleData(localePt);
 
@@ -44,6 +45,7 @@ registerLocaleData(localePt);
     ProductCreateComponent,
     ProductReadComponent,
     ProductUpdateComponent,
+    ProductDeletComponent,
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/products/product-delet/product-delet.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-delet',
+  templateUrl: './product-delet.component.html',
+  styleUrls: ['./product-delet.component.css']
+})
+export class ProductDeletComponent {
+
+}
