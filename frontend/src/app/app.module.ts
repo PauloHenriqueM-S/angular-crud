3a9bbb553036b523c7a2ad5c4709c2c288776259
Paulo Henrique <phttm0501@gmail.com>
2023-10-29 18:25:31 -0400

    Delete created item and updated service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -27,8 +27,8 @@ import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatSortModule } from "@angular/material/sort";
 import localePt from "@angular/common/locales/pt";
 import { registerLocaleData } from "@angular/common";
-import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
-import { ProductDeletComponent } from './components/products/product-delet/product-delet.component';
+import { ProductUpdateComponent } from "./components/product/product-update/product-update.component";
+import { ProductDeleteComponent } from "./components/product/product-delete/product-delete.component";
 
 registerLocaleData(localePt);
 
@@ -45,7 +45,7 @@ registerLocaleData(localePt);
     ProductCreateComponent,
     ProductReadComponent,
     ProductUpdateComponent,
-    ProductDeletComponent,
+    ProductDeleteComponent,
   ],
   imports: [
     BrowserModule,
